bade5fcfccead8c815674bf77aa34a1e30205609
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-02-15 19:20:55 +0000

    Fixed the console, added TODOs

--- Web Proxy/src/proxy/main.go
+++ Web Proxy/src/proxy/main.go
@@ -1,8 +1,8 @@
 package main 
 
 import (
-	//"net/http"
-	//"time"
+	"net/http"
+	"time"
 	"golang.org/x/crypto/ssh/terminal"
 	"io"
 	"log"
@@ -16,21 +16,21 @@ type ReadWriter struct {
 	io.Reader
 	io.Writer
 }
-// TODO Make a channel for displaying and sending messaged to/from main program
+// TODO Make a channel for displaying and sending messaged to/from main program, maybe make this function just reader
 // This function makes a terminal for our web proxy
 func makeTerminal() {
 	rw := ReadWriter{
 		Reader: os.Stdin,
 		Writer: os.Stdout,
 	}
-	// uncomment this part if using unix/cmd
+	// uncomment this part if using unix like console
 //	oldState, err := terminal.MakeRaw(int(syscall.Stdin))
 //	if err != nil {
 //        log.Fatal(err)
 //	}
 //	defer terminal.Restore(int(syscall.Stdin), oldState)
 	
-	term := terminal.NewTerminal(rw,">")
+	term := terminal.NewTerminal(rw,"WebProxy >")
 	// test
 	for {
 		line, err := term.ReadLine()
@@ -46,28 +46,27 @@ func makeTerminal() {
 		fmt.Fprintln(term, line)
 	}
 }
-
+// TODO make a handler for requests and a map to store them.
 func main() {
 	
 	go makeTerminal()
 	
-	
-	for{}
+
 //	tr := &http.Transport{
 //		MaxIdleConns:       10,
 //		IdleConnTimeout:    30 * time.Second,
 //		DisableCompression: true,
 //	}
-//	client := &http.Client{ Transport: tr }
-//	
-//	s := &http.Server{
-//		Addr:           ":8080",
-//		Handler:        ,
-//		ReadTimeout:    10 * time.Second,
-//		WriteTimeout:   10 * time.Second,
-//		MaxHeaderBytes: 1 << 20,
-//	}
-//	log.Fatal(s.ListenAndServe())
+//	//client := &http.Client{ Transport: tr }
+	
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 	
 
 }
