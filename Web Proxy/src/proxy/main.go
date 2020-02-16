2e74b7cf99424885b18251a93caebc218a7f0708
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-02-16 21:22:16 +0000

    Made Http clone forwarding, made https forwarding, but need to fix weird TLS handshake error.

--- Web Proxy/src/proxy/main.go
+++ Web Proxy/src/proxy/main.go
@@ -1,21 +1,28 @@
 package main 
 
 import (
+	"net"
 	"net/http"
 	"time"
 	"golang.org/x/crypto/ssh/terminal"
 	"io"
+	"io/ioutil"
 	"log"
 	//"encoding/json"
 	"os"
 	//"syscall"
 	"fmt"
+	//"crypto/tls"
+	//"crypto/x509"
 )
 
 type ReadWriter struct {
 	io.Reader
 	io.Writer
 }
+
+//var cache = map[http.Request]http.Response
+
 // TODO Make a channel for displaying and sending messaged to/from main program, maybe make this function just reader
 // This function makes a terminal for our web proxy
 func makeTerminal() {
@@ -23,7 +30,7 @@ func makeTerminal() {
 		Reader: os.Stdin,
 		Writer: os.Stdout,
 	}
-	// uncomment this part if using unix like console
+	// uncomment this part if using unix like console and uncomment syscall package
 //	oldState, err := terminal.MakeRaw(int(syscall.Stdin))
 //	if err != nil {
 //        log.Fatal(err)
@@ -49,25 +56,87 @@ func makeTerminal() {
 // TODO make a handler for requests and a map to store them.
 func main() {
 	
-	go makeTerminal()
+	//go makeTerminal()
+	certFile := "src/proxy/cert.pem"
+	keyFile := "src/proxy/key.pem"
+//	go log.Fatal(http.ListenAndServe(":42069",http.HandlerFunc(httpRequestHandler)))
+	log.Fatal(http.ListenAndServeTLS(":42070",certFile, keyFile, http.HandlerFunc(httpsRequestHandler)))
+	//log.Printf("Listeners set up successfully")
 	
-
-//	tr := &http.Transport{
-//		MaxIdleConns:       10,
-//		IdleConnTimeout:    30 * time.Second,
-//		DisableCompression: true,
-//	}
-//	//client := &http.Client{ Transport: tr }
-	
-	s := &http.Server{
-		Addr:           ":8080",
-		Handler:        nil,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	log.Fatal(s.ListenAndServe())
-	
-
 }
 
+//TODO finish this fucntion
+// This function handles incoming requests and responds to them if needed
+func httpRequestHandler(w http.ResponseWriter, req *http.Request) {
+	client := &http.Client{}
+	
+	log.Printf("received the request")
+	url := req.URL
+    url.Host = req.Host
+    url.Scheme = "http"
+    
+	// clone and forward
+	proxyReq, err := http.NewRequest(req.Method, url.String(), req.Body)
+	if err != nil {
+		log.Print(err)
+	}
+	proxyReq.Header = req.Header
+	resp, err := client.Do(proxyReq)
+	if err != nil {
+		log.Printf("Error sending forwarding request %v",err)
+	}
+	log.Printf("forwarded the request and received the response")	
+	
+	// clone the response and send to client
+	body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    log.Printf("Read the body")
+	for head, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(head, value)
+		}
+	}
+	w.Write(body)
+	if(resp.StatusCode != 200) {
+		w.WriteHeader(resp.StatusCode)
+	}
+	log.Printf("Finished redirection")		
+}
+
+func httpsRequestHandler(w http.ResponseWriter, req *http.Request) {
+	log.Printf("received https request")
+	if req.Method != http.MethodConnect {
+        http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+        return
+    }
+	log.Printf("Dialing...")
+	destConn, err := net.DialTimeout("tcp", req.Host, time.Second*30)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusServiceUnavailable)
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    hijacker, ok := w.(http.Hijacker)
+    if !ok {
+        http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+        return
+    }
+    log.Printf("Conncetion Hijacked")
+    clientConn, _, err := hijacker.Hijack()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusServiceUnavailable)
+        return
+    }
+    go transfer(destConn, clientConn)
+    go transfer(clientConn, destConn)
+    log.Printf("Finished tunelling")
+}
+
+func transfer(dest io.WriteCloser, src io.ReadCloser) {
+    defer func() { _ = dest.Close() }()
+    defer func() { _ = src.Close() }()
+    _, _ = io.Copy(dest, src)
+}
