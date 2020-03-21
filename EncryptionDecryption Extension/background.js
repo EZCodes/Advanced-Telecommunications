c27f695f53c367eedcf0469068dadb8b79266580
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-03-21 22:09:46 +0000

    Fixed the popups, and added changing between them

--- EncryptionDecryption Extension/background.js
+++ EncryptionDecryption Extension/background.js
@@ -1,6 +1,6 @@
  chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+      console.log("Entered Background File");
     });
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
      chrome.declarativeContent.onPageChanged.addRules([{
@@ -11,6 +11,12 @@
            actions: [new chrome.declarativeContent.ShowPageAction()]
      }]);
     });
+	//chrome.runtime.onMessage.addListener(
+	//	function(message) {
+	//		if(message.type == "link") {
+	//			window.location.href = message.link;
+	//		}
+	//	});
 	//chrome.storage.local.get('signed_in', function(data) {
 	//  if (data.signed_in) {
     //    chrome.browserAction.setPopup({popup: 'popup.html'});
--- EncryptionDecryption Extension/popup.js
+++ EncryptionDecryption Extension/popup.js
@@ -1,14 +1,30 @@
-  let changeColor = document.getElementById('changeColor');
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-  });
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+function openGroupAdder() {
+	console.log("Entered Add To Group");
+	chrome.browserAction.setPopup({popup: 'add_popup.html'});
+	window.location.href = "add_popup.html";
+}
+
+function openGroupRemover() {
+	console.log("Entered Remove From Group");
+	chrome.browserAction.setPopup({popup: 'remove_popup.html'});
+	window.location.href = "remove_popup.html";
+}
+
+function openEncryptor() {
+	console.log("Entered Encryption");
+	chrome.browserAction.setPopup({popup: 'popup.html'});
+	window.location.href = "popup.html";
+}
+
+function openDecryptor() {
+	console.log("Entered Decryption");
+	chrome.browserAction.setPopup({popup: 'decrypt_popup.html'});
+	window.location.href = "decrypt_popup.html";
+}
+
+// Listeners for the buttons
+document.getElementById('addToGroup').addEventListener('click', openGroupAdder);
+document.getElementById('removeFromGroup').addEventListener('click', openGroupRemover);
+document.getElementById('encryptionMode').addEventListener('click', openEncryptor);
+document.getElementById('decryptionMode').addEventListener('click', openDecryptor);
