d10d4a26a613be7cef2ca1114047a7110d4ef71c
Jerzy "George" Jaśkuć <38333392+EZCodes@users.noreply.github.com>
2020-03-22 13:31:20 +0000

    Added text fields and submit fields
    Also started working on a backend that will be in golang

---
+++ Encryption-Decryption/EncryptionDecryption Extension/popup.js
@@ -0,0 +1,37 @@
+
+function openGroupAdder() {
+	console.log("Entered Add To Group");
+	chrome.browserAction.setPopup({popup: 'add_popup.html'});
+	window.location.href = "add_popup.html";
+}
+
+function openGroupRemover() {
+	console.log("Entered Remove From Group");
+	chrome.browserAction.setPopup({popup: 'remove_popup.html'});
+	window.location.href = "remove_popup.html";
+}
+
+function openEncryptor() {
+	console.log("Entered Encryption");
+	chrome.browserAction.setPopup({popup: 'popup.html'});
+	window.location.href = "popup.html";
+}
+
+function openDecryptor() {
+	console.log("Entered Decryption");
+	chrome.browserAction.setPopup({popup: 'decrypt_popup.html'});
+	window.location.href = "decrypt_popup.html";
+}
+
+function parseMessage() {
+	var text = document.getElementById('encryp').value;
+	var recipients = document.getElementById('recipient').value
+	document.getElementById("output").innerHTML = "Success!!"
+}
+
+// Listeners for the buttons
+document.getElementById('addToGroup').addEventListener('click', openGroupAdder);
+document.getElementById('removeFromGroup').addEventListener('click', openGroupRemover);
+document.getElementById('encryptionMode').addEventListener('click', openEncryptor);
+document.getElementById('decryptionMode').addEventListener('click', openDecryptor);
+document.getElementById('send').addEventListener('click', parseMessage);
